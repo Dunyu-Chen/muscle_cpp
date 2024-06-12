a559b275789f88b78703205f6dc550ad3653ff03
dunyu <dunyuchen@live.com>
2024-06-12 23:32:47 +0800

    init lib_signal with class Signal and instance sin_signal

--- include/muscle_cpp/lib_signal.h
+++ include/muscle_cpp/lib_signal.h
@@ -5,11 +5,44 @@
 #ifndef MUSCLE_CPP_LIB_SIGNAL_H
 #define MUSCLE_CPP_LIB_SIGNAL_H
 #include <vector>
+#include <chrono>
+
+struct Node_Sample_Time{
+    double period; // in us
+    std::chrono::microseconds chrono_us{};
+    std::chrono::milliseconds chrono_ms{};
+};
+
+Node_Sample_Time init_node_sample_time(double Period);
+
 class Signal{
 public:
-    double sample_time;
+    virtual double search (double Time_stamp);
+    Node_Sample_Time sample_time;
+    std::vector<double> time_stamp_list;
+    double time_stamp;
+    std::vector<uint> sequence_list;
+    uint sequence;
+    double signal_data;
+    std::vector<double> signal_data_list;
+    void reset();
+    explicit Signal(double Sample_Period);
 
 private:
 
+};
+
+struct Signal_sin_Params {
+    double Amp;
+    double pha;
+    double Fre;
+    double Margin;
+};
+
+class Signal_sin : Signal {
+public:
+    Signal_sin(double Sample_Period, Signal_sin_Params Params);
+    Signal_sin_Params Params;
+    double search(double Time_Stamp) override;
 };
 #endif //MUSCLE_CPP_LIB_SIGNAL_H
--- src/lib_signal.cpp
+++ src/lib_signal.cpp
@@ -1,4 +1,44 @@
 //
 // Created by dunyu on 24-6-12.
 //
+#include <cmath>
+#include "muscle_cpp/lib_signal.h"
 
+Node_Sample_Time init_node_sample_time(double Period){
+    Node_Sample_Time return_struct;
+    return_struct.period = Period; // S
+    return_struct.chrono_us = std::chrono::microseconds(static_cast<int>(std::round(Period * 1000000.0)));
+    return_struct.chrono_ms = std::chrono::milliseconds (static_cast<int>(std::round(Period * 1000.0)));
+    return return_struct;
+}
+
+Signal::Signal(double Sample_Period) {
+    this->sample_time = init_node_sample_time(Sample_Period);
+    this->reset();
+}
+
+void Signal::reset() {
+    sequence_list ={};
+    sequence = 0;
+    time_stamp = 0.0;
+    time_stamp_list={};
+    signal_data_list={};
+    signal_data = this->search(time_stamp);
+}
+
+double Signal::search (double Time_stamp){
+    return 0;
+}
+
+Signal_sin::Signal_sin(
+    double Sample_Period,
+    Signal_sin_Params Params)
+    : Signal(Sample_Period) {
+
+}
+
+double Signal_sin::search(double Time_Stamp) {
+    double return_value;
+    return_value = this->Params.Amp * sin(this->Params.Fre * Time_Stamp + this->Params.pha) + this->Params.Margin;
+    return return_value;
+}
