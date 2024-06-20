33b9903d0009c906f1f28debab641b48734c13af
dunyu <dunyuchen@live.com>
2024-06-20 17:53:49 +0800

    add matplotlib ploting library and test it

--- src/test_lib_signal.cpp
+++ src/test_lib_signal.cpp
@@ -3,6 +3,10 @@
 //
 
 #include "muscle_cpp/lib_signal.h"
+#include "muscle_cpp/matplotlibcpp.h"
+#include "vector"
+
+namespace plt = matplotlibcpp;
 
 int main(int argc, char * argv[]){
     Signal_sin_Params params{};
@@ -13,19 +17,22 @@ int main(int argc, char * argv[]){
 
     Signal_sin test_signal(0.001,params) ;
 
-    for (int i = 0; i < 100; i++){
+    for (int i = 0; i < 10000; i++){
         test_signal.step();
     }
 
     Signal_step_Params params1{};
-    params1.time_coordinates = {0,0.4,0.8};
-    params1.signal_coordinates = {1,4,8};
-    params1.period = 0.01; //s
+    params1.time_coordinates = {0,0.2,0.4,0.8};
+    params1.signal_coordinates = {0,2,4,8};
+    params1.period = 1; //s
 
     Signal_step test_signal2(0.001,params1);
-    for (int i = 0; i<100;i++){
+    for (int i = 0; i<10000;i++){
         test_signal2.step();
     }
 
+    //plt::plot<>(test_signal.time_stamp_list,test_signal.signal_data_list);
+    plt::plot<>(test_signal2.time_stamp_list,test_signal2.signal_data_list);
+    plt::show();
     return 0;
 }
